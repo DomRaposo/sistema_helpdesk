f1127e54d4ebebb10b6afd5ae25e84646bcfeb0b
unknown <DomRaposo>
2025-05-25 13:11:14 -0300

    subindo respostasChamado

--- app/Enums/StatusChamadoEnum.php
+++ app/Enums/StatusChamadoEnum.php
@@ -6,4 +6,9 @@ enum StatusChamadoEnum: string
     case ABERTO = 'ABERTO';
     case EM_ATENDIMENTO = 'EM_ATENDIMENTO';
     case ENCERRADO = 'ENCERRADO';
+
+    public static function values(): array
+    {
+        return array_column(self::cases(), 'value');
+    }
 }
--- app/Repositories/RespostaRepository.php
+++ app/Repositories/RespostaRepository.php
@@ -2,7 +2,34 @@
 
 namespace App\Repositories;
 
+use App\Models\Resposta;
+
 class RespostaRepository
 {
+    public function getAll()
+    {
+        return Resposta::all();
+    }
+
+    public function find($id)
+    {
+        return Resposta::find($id);
+    }
+
+    public function create(array $data)
+    {
+        return Resposta::create($data);
+    }
+
+    public function update($resposta, $data)
+    {
+        return $resposta->update($data);
+    }
+
+    public function delete($resposta)
+    {
+        return $resposta->delete();
+    }
+
 
 }
